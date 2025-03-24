933039145f638427c0c4345e6afe003681f4b198
Loka <loka@iim.ai>
2025-03-24 18:37:57 +0800

    集成/v1/responses

--- model/openai.go
+++ model/openai.go
@@ -1,9 +1,14 @@
 package model
 
 type OpenAIResponsesReq struct {
+	Model  string `json:"model,omitempty"`
+	Input  any    `json:"input"`
+	Stream bool   `json:"stream,omitempty"`
 }
 
 type OpenAIResponsesRes struct {
+	Model string          `json:"model"`
+	Usage *ResponsesUsage `json:"usage"`
 	Error *struct {
 		Message string `json:"message"`
 		Type    string `json:"type"`
@@ -15,4 +20,16 @@ type OpenAIResponsesRes struct {
 	Duration      int64  `json:"-"`
 	TotalTime     int64  `json:"-"`
 	Err           error  `json:"-"`
+}
+
+type ResponsesUsage struct {
+	InputTokens        int `json:"input_tokens"`
+	InputTokensDetails struct {
+		CachedTokens int `json:"cached_tokens"`
+	} `json:"input_tokens_details"`
+	OutputTokens        int `json:"output_tokens"`
+	OutputTokensDetails struct {
+		ReasoningTokens int `json:"reasoning_tokens"`
+	} `json:"output_tokens_details"`
+	TotalTokens int `json:"total_tokens"`
 }
